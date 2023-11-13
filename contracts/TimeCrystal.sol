6b4ee4c13d092c2ebb9549b6780682c1b10d4fd3
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-11-13 00:07:58 -0500

    Update TimeCrystal.sol

--- contracts/TimeCrystal.sol
+++ contracts/TimeCrystal.sol
@@ -1,1 +1,222 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract RemixTester is FunctionsClient, ConfirmedOwner {
+  using FunctionsRequest for FunctionsRequest.Request;
+
+  bytes32 public donId;
+
+  bytes32 public s_lastRequestId;
+  bytes public s_lastResponse;
+  bytes public s_lastError;
+
+  string public send_source;
+  string public decrypt_source;
+  FunctionsRequest.Location public secretsLocation;
+  bytes encryptedSecretsReference;
+  uint64 subscriptionId = 1600;
+  uint32 callbackGasLimit = 300000;
+  
+
+  constructor(address router, bytes32 _donId, string memory _source,  string memory _source2, FunctionsRequest.Location _location, bytes memory _reference) FunctionsClient(router) ConfirmedOwner(msg.sender) {
+    donId = _donId;
+    send_source = _source;
+    decrypt_source = _source2;
+    secretsLocation = _location;
+    encryptedSecretsReference = _reference;
+  }
+
+  enum requestType {
+    SEND,
+    RECEIVE,
+    DECRYPT,
+    TAKE_TURN,
+    START_GAME
+  }
+
+  struct encryptParams {
+    uint seed;
+    uint nonce;
+    string counter;
+  }
+  
+  mapping (address => bool) public inSession;
+
+  mapping (address => uint) public sessionIndex;
+  mapping (address => encryptParams[]) public sessions;
+
+  mapping (bytes32 => address) public requestIdbyRequester;
+
+  mapping (address => bytes) public returnedSecret;
+
+  uint public nonceCounter = 1;
+  mapping (bytes32 => requestType) public pendingRequests;
+  mapping (uint => bool) public usedSeeds;
+  mapping (string => bool) usedCounters;
+
+  mapping (address => string) registeredKeys;
+  mapping (address => bool) playerKeyRegistered;
+
+  mapping (address => bytes) public decryptedMessage;
+
+
+//             NEW GAME STUFF             //    
+
+    //temp
+    string[3] public currentOpponent;
+    uint8 public currentTurn = 0;
+    string[] public opponentCards;
+    string[] public playerCards;
+    uint gameSeed;
+    string gameCounter;
+    string gameNonce = "1";
+    string playerDeck;
+
+    function registerPlayerDeck(string calldata _deck) public {
+        playerDeck = _deck;
+    }
+
+    //RSA-encrypted AES key, AES-encrypted deck/logic, and the iv used to encrypt
+    //all provided as base64 strings
+    function registerOpponentDeck(string calldata _key, string calldata _deck, string calldata _iv) public {
+        string[3] memory newOpponent;
+        newOpponent[0] = _key;
+        newOpponent[1] = _deck;
+        newOpponent[2] = _iv;
+        currentOpponent = newOpponent;
+    }
+
+
+    //counter here is a base64 string
+    function startGame (uint _seed, string calldata _counter) external {
+
+    FunctionsRequest.Request memory req;
+    req.initializeRequest(FunctionsRequest.Location.Inline, FunctionsRequest.CodeLanguage.JavaScript, send_source);
+    req.secretsLocation = secretsLocation;
+    req.encryptedSecretsReference = encryptedSecretsReference;
+    
+    gameSeed = _seed;
+    gameCounter = _counter;
+    
+    string[] memory args = new string[](4);
+    args[0] = Strings.toString(_seed);
+    args[1] = _counter;
+    args[2] = gameNonce;
+    args[3] = playerDeck;
+
+    req.setArgs(args);
+    //req.setBytesArgs(args);
+
+    s_lastRequestId = _sendRequest(req.encodeCBOR(), subscriptionId, callbackGasLimit, donId);
+    requestIdbyRequester[s_lastRequestId] = msg.sender;
+    pendingRequests[s_lastRequestId] = requestType.START_GAME;
+
+
+  }
+
+    //seed and counter will eventually be replaced
+    function progressGame (uint8 _action) external {
+
+    FunctionsRequest.Request memory req;
+    req.initializeRequest(FunctionsRequest.Location.Inline, FunctionsRequest.CodeLanguage.JavaScript, send_source);
+    req.secretsLocation = secretsLocation;
+    req.encryptedSecretsReference = encryptedSecretsReference;
+
+    
+    string[] memory args = new string[](8);
+    args[0] = playerCards[_action];
+    args[1] = currentOpponent[0];
+    args[2] = currentOpponent[1];
+    args[3] = currentOpponent[2];
+    args[4] = Strings.toString(currentTurn);
+    args[5] = Strings.toString(gameSeed);
+    args[6] = gameCounter;
+    args[7] = gameNonce;
+
+    playerCards[_action] = playerCards[playerCards.length - 1];
+    playerCards.pop();
+
+    currentTurn += 1;
+
+    req.setArgs(args);
+    //req.setBytesArgs(args);
+
+    s_lastRequestId = _sendRequest(req.encodeCBOR(), subscriptionId, callbackGasLimit, donId);
+    requestIdbyRequester[s_lastRequestId] = msg.sender;
+    pendingRequests[s_lastRequestId] = requestType.TAKE_TURN;
+
+
+  }
+
+    function getPlayerCards() public view returns (string[] memory) {
+        return playerCards;
+    }
+
+    function getOpponentCards() public view returns (string[] memory) {
+        return opponentCards;
+    }
+
+
+
+
+//            REQUEST FULFILLMENT            //
+
+  // SEND: send encrypted message to DON
+  // RECEIVE: receive encrypted message from DON
+  // DECRYPT: decrypt secret randomness
+  function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory err) internal override {
+    if (pendingRequests[requestId] == requestType.SEND) {
+        address player = requestIdbyRequester[requestId];
+        decryptedMessage[player] = response;
+
+    }
+    else if (pendingRequests[requestId] == requestType.DECRYPT) {
+        address player = requestIdbyRequester[requestId];
+        inSession[player] = false;
+        sessionIndex[player] += 1;
+        returnedSecret[player] = response;
+    }
+    else if (pendingRequests[requestId] == requestType.START_GAME) {
+        (string memory playerCard1, string memory playerCard2, string memory playerCard3) = abi.decode(response, (string, string, string));
+        string[3] memory newCards;
+        newCards[0] = playerCard1;
+        newCards[1] = playerCard2;
+        newCards[2] = playerCard3;
+        playerCards = newCards;
+    }
+    else if (pendingRequests[requestId] == requestType.TAKE_TURN) {
+        (string memory opponentCard, string memory playerCard) = abi.decode(response, (string, string));
+        opponentCards.push(opponentCard);
+        playerCards.push(playerCard);
+    }
+    
+    s_lastResponse = response;
+    s_lastError = err;
+    emit RequestFulfilled(requestId, response);
+  }
+
+
+
+
+
+  /**
+   * @notice Set the DON ID
+   * @param newDonId New DON ID
+   */
+  function setDonId(bytes32 newDonId) external onlyOwner {
+    donId = newDonId;
+  }
+
+  function updateSecret(bytes calldata _secrets) external onlyOwner {
+    encryptedSecretsReference = _secrets;
+  }
+
+
+  event RequestFulfilled(bytes32 indexed _id, bytes indexed _response);
+  
+}
