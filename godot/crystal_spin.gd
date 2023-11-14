24f04896eae333eb7820c67aadfd35702ba79ff9
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-11-13 20:35:20 -0500

    Update crystal_spin.gd

--- godot/crystal_spin.gd
+++ godot/crystal_spin.gd
@@ -1,14 +1,9 @@
 extends MeshInstance
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass 
 
 
 var grow_time = 0
