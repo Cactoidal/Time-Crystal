18ad23f625f5ee35644068d9c86eb7563d17b5ab
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-11-15 22:06:29 -0500

    Update TimeCrystal.sol

--- contracts/TimeCrystal.sol
+++ contracts/TimeCrystal.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/FunctionsClient.sol";
 import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
 import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/dev/v1_0_0/libraries/FunctionsRequest.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "./ILogAutomation.sol";
+import "ILogAutomation.sol";
 
 contract RemixTester is FunctionsClient, ConfirmedOwner {
   using FunctionsRequest for FunctionsRequest.Request;
@@ -65,6 +65,25 @@ contract RemixTester is FunctionsClient, ConfirmedOwner {
     string gameNonce = "1";
     string public playerDeck;
 
+    enum validationType {
+    PLAYER,
+    OPPONENT
+  }
+
+    struct gameSession {
+    uint8 playerHealth;
+    uint8 opponentHealth;
+    uint8[] playerCards;
+    uint8[] opponentCards;
+    bytes updateBytes;
+    validationType updateType;
+    bool updateInFlight;
+  }
+
+    mapping (address => bool) inSession;
+    mapping (address => gameSession) currentSession;
+    mapping (address => bool) awaitingAutomation;
+
     function registerPlayerDeck(string calldata _deck) public {
         playerDeck = _deck;
     }
@@ -89,6 +108,12 @@ contract RemixTester is FunctionsClient, ConfirmedOwner {
     //require (usedSeeds[_seed] == false);
     //usedSeeds[_seed] = true;
     //usedCounters[_counter] = true;
+    require (inSession[msg.sender] == false);
+    inSession[msg.sender] = true;
+
+    currentSession[msg.sender].playerHealth = 15;
+    currentSession[msg.sender].opponentHealth = 15;
+
     FunctionsRequest.Request memory req;
     req.initializeRequest(FunctionsRequest.Location.Inline, FunctionsRequest.CodeLanguage.JavaScript, start_game_source);
     req.secretsLocation = secretsLocation;
@@ -109,11 +134,21 @@ contract RemixTester is FunctionsClient, ConfirmedOwner {
     s_lastRequestId = _sendRequest(req.encodeCBOR(), subscriptionId, callbackGasLimit, donId);
     requestIdbyRequester[s_lastRequestId] = msg.sender;
     pendingRequests[s_lastRequestId] = requestType.START_GAME;
-
-
   }
 
-    function progressGame (uint8 _action) external {
+
+  function playerTakeTurn (bytes memory _actions) external {
+    require (inSession[msg.sender] == true);
+    require (currentSession[msg.sender].updateInFlight == false);
+    currentSession[msg.sender].updateInFlight = true;
+    currentSession[msg.sender].updateType = validationType.PLAYER;
+    currentSession[msg.sender].updateBytes = _actions;
+    emit AwaitingOpponentValidation(msg.sender);
+  }
+
+
+    //time to move this into checkUpkeep
+    function progressGame (uint8 _action, address _player) internal {
 
     FunctionsRequest.Request memory req;
     req.initializeRequest(FunctionsRequest.Location.Inline, FunctionsRequest.CodeLanguage.JavaScript, take_turn_source);
@@ -141,7 +176,7 @@ contract RemixTester is FunctionsClient, ConfirmedOwner {
     //req.setBytesArgs(args);
 
     s_lastRequestId = _sendRequest(req.encodeCBOR(), subscriptionId, callbackGasLimit, donId);
-    requestIdbyRequester[s_lastRequestId] = msg.sender;
+    requestIdbyRequester[s_lastRequestId] = _player;
     pendingRequests[s_lastRequestId] = requestType.TAKE_TURN;
 
 
@@ -185,18 +220,10 @@ contract RemixTester is FunctionsClient, ConfirmedOwner {
     }
 
     else if (pendingRequests[requestId] == requestType.TAKE_TURN) {
-        //string memory raw = abi.decode(response, (string));
         address player = requestIdbyRequester[requestId];
         currentSession[player].updateBytes = response;
-        emit WaitingForUpkeep(player);
-        //bytes memory opponentCard = new bytes(2);
-        //bytes memory playerCard = new bytes(2);
-        //opponentCard[0] = response[0];
-        //opponentCard[1] = response[1];
-        //playerCard[0] = response[2];
-        //playerCard[1] = response[3];
-        //opponentCards.push(string(opponentCard));
-        //playerCards.push(string(playerCard));
+        currentSession[player].updateType = validationType.OPPONENT;
+        emit AwaitingOpponentValidation(player);
     }
     
     s_lastResponse = response;
@@ -230,28 +257,70 @@ contract RemixTester is FunctionsClient, ConfirmedOwner {
     cardKeyword traitB;
   }
 
-  mapping (uint => cardTraits) cards;
-
-  struct gameSession {
-    uint8 playerHealth;
-    uint8 opponentHealth;
-    uint8[] playerCards;
-    uint8[] opponentCards;
-    bytes updateBytes;
-  }
-
-  mapping (address => bool) inSession;
-  mapping (address => gameSession) currentSession;
-  mapping (address => bool) awaitingAutomation;
-
+  mapping (string => cardTraits) cards;
 
   function checkLog(
     Log calldata log,
     bytes memory checkData
   ) external view returns (bool upkeepNeeded, bytes memory performData) {
-        
         address player = address(uint160(uint256(log.topics[1])));
+        
         bytes memory cardBytes = currentSession[player].updateBytes;
+        validationType updateType = currentSession[player].updateType;
+        string memory playerDrawnCard;
+
+        //decode lead byte for action count
+        uint8 actionCount = uint8(cardBytes[0]);
+
+        //sort actions
+        bytes[] memory actions = new bytes[](actionCount);
+        uint8 index = 1;
+        for (uint i = 0; i < actionCount; i++) {
+            bytes memory card = new bytes(5);
+            for (uint j = 0; j < 5; j++) {
+                card[j] = cardBytes[index];
+                index += 1;
+            }
+            actions[i] = (card);
+            //draw the player card
+            if (updateType == validationType.OPPONENT) {
+                bytes memory playerCard;
+                playerCard[0] = cardBytes[index];
+                index += 1;
+                playerCard[1] = cardBytes[index];
+                playerDrawnCard = string(card);
+            }
+        }
+
+            //validate and load actions into phases (PLAY CARD, BLOCK, ATTACK)
+            for (uint k = 0; k < actionCount; k++) {
+                bytes memory card = new bytes(2);
+                bytes memory target = new bytes(2);
+                bytes memory action = new bytes(1);
+                card[0] = actions[k][0];
+                card[1] = actions[k][1];
+                target[0] = actions[k][2];
+                target[1] = actions[k][3];
+                action[0] = actions[k][4];
+                if (cards[string(card)].cardType == cType.CONSTRUCT) {
+
+
+                }
+
+
+            }
+
+            //execute phases
+
+        if (currentSession[player].updateType == validationType.PLAYER) {
+            
+
+            //write the game state into a JSON to give to Functions DON
+
+        }
+
+
+        if (currentSession[player].updateType == validationType.OPPONENT) {
         bytes memory opponentCard = new bytes(2);
         bytes memory playerCard = new bytes(2);
         opponentCard[0] = cardBytes[0];
@@ -261,7 +330,7 @@ contract RemixTester is FunctionsClient, ConfirmedOwner {
         upkeepNeeded = true;
         performData = abi.encode(string(opponentCard), string(playerCard));
         return (upkeepNeeded, performData);
-
+        }
   }
 
 
@@ -270,6 +339,10 @@ contract RemixTester is FunctionsClient, ConfirmedOwner {
         bytes calldata performData
     ) external {
        // require(msg.sender == forwarder);
+        
+        
+        
+        
         (string memory opponentCard, string memory playerCard) = abi.decode(performData, (string, string));
         opponentCards.push(opponentCard);
         playerCards.push(playerCard);
@@ -299,11 +372,9 @@ contract RemixTester is FunctionsClient, ConfirmedOwner {
 
 
 
-
-
-
   event RequestFulfilled(bytes32 indexed _id, bytes indexed _response);
-  event WaitingForUpkeep(address indexed _player);
+  event AwaitingPlayerValidation(address indexed _player);
+  event AwaitingOpponentValidation(address indexed _player);
   event UpkeepFulfilled(bytes indexed _performData);
 
 
