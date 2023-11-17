6417c6007f01b910aa8408d256a58a1fb317b943
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-11-16 22:42:17 -0500

    Update GameLogic.sol

--- contracts/GameLogic.sol
+++ contracts/GameLogic.sol
@@ -1,1 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import "ILogAutomation.sol";
+
+contract GameLogic is ConfirmedOwner {
+
+    constructor() ConfirmedOwner(msg.sender) {
+
+    }
+
+    address forwarder;
+    address game;
+
+    function setForwarder(address _forwarder) external onlyOwner {
+        forwarder = _forwarder;
+    }
+
+    function setGameContract(address _game) external onlyOwner {
+        game = _game;
+    }
+
+
+
+
+    function checkLog(
+    Log calldata log,
+    bytes memory checkData
+  ) public {
+
+  }
+
+  function performUpkeep(
+        bytes calldata performData
+    ) external {
+    require(msg.sender == forwarder);
+    
+    }
+
+}
