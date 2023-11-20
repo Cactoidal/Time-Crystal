50cc02933c515955d96884aa92e14a545bdde3c6
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-11-20 04:27:49 -0500

    Update IGameLogic.sol

--- contracts/IGameLogic.sol
+++ contracts/IGameLogic.sol
@@ -3,6 +3,10 @@ pragma solidity 0.8.20;
 
 interface IGameLogic {
 
+    function checkHandSize(bytes memory _hand) external pure returns (bool);
+
+    function getData(bytes memory hand, bytes memory actions) external pure returns (uint8, uint8, bytes[] memory, bytes[] memory, bytes[] memory);
+
     function checkHandActions (uint8 leadByte, bytes[] memory _hand, bytes[] memory handActions, bytes[] memory playerField, bytes[] memory opponentField) external view returns (bool);
 
     function checkFieldActions (uint8 leadByte, bytes[] memory fieldActions, bytes[] memory _playerField, bytes[] memory opponentField) external pure returns (bool);
