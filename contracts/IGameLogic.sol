b0fd5a0e278fdee1bd774cad04f7b8e9346ea936
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-11-20 04:51:12 -0500

    Update IGameLogic.sol

--- contracts/IGameLogic.sol
+++ contracts/IGameLogic.sol
@@ -11,6 +11,9 @@ interface IGameLogic {
 
     function checkFieldActions (uint8 leadByte, bytes[] memory fieldActions, bytes[] memory _playerField, bytes[] memory opponentField) external pure returns (bool);
 
-    function doHandActions (uint8 leadByte, bytes[] memory handActions, bytes[] memory _playerField, bytes[] memory _opponentField) external view returns (bytes[] memory);
+    function doHandActions (uint8 leadByte, bytes[] memory handActions, bytes[] memory _playerField, bytes[] memory _opponentField) external view returns (bytes[] memory, uint8[4] memory, uint8[4] memory, uint8);
 
+    function doFieldActions (uint8 leadByte, bytes[] memory fieldActions, bytes[] memory _playerField, bytes[] memory _opponentField, uint8[4] memory _destructible, uint8[4] memory _damaged, uint8 destructionIndex) external view returns (bytes[] memory, uint8[4] memory, uint8[4] memory);
+
+    function applyEffects (bytes[] memory _playerField, bytes[] memory _opponentField, uint8[4] memory _destructible, uint8[4] memory _damaged, uint8 playerHealth, uint8 opponentHealth) external view returns (bytes[] memory, bytes[] memory, uint8, uint8);
 }
