bccc0a7ee159bcad7e71693c3ecdabab69ee3da6
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-11-26 01:21:56 -0500

    Update lib.rs

--- godot/rust/lib.rs
+++ godot/rust/lib.rs
@@ -274,10 +274,74 @@ NewFuture(Ok(()))
 }
 
 
+#[method]
+#[tokio::main]
+async fn commit_action(key: PoolArray<u8>, chain_id: u64, time_crystal_contract: GodotString, rpc: GodotString, _gas_fee: u64, _count: u64, _secrets: GodotString, ui_node: Ref<Spatial>) -> NewFuture {
+
+let vec = &key.to_vec();
+
+let keyset = &vec[..]; 
+             
+let prewallet : LocalWallet = LocalWallet::from_bytes(&keyset).unwrap();
+        
+let wallet: LocalWallet = prewallet.with_chain_id(chain_id);
+        
+let user_address = wallet.address();
+        
+let provider = Provider::<Http>::try_from(rpc.to_string()).expect("could not instantiate HTTP Provider");
+        
+let contract_address: Address = time_crystal_contract.to_string().parse().unwrap();
+        
+let client = SignerMiddleware::new(provider, wallet.clone());
+        
+let contract = TimeCrystalABI::new(contract_address.clone(), Arc::new(client.clone()));
+
+let secrets: &str = &_secrets.to_string();
+
+let encoded = ethers::abi::AbiEncode::encode(secrets);
+
+let mut sha =  openssl::sha::Sha256::new();
+
+sha.update(&encoded);
+
+let hashed = sha.finish();
+
+let bytes: ethers::types::Bytes = hashed.into();
+
+let calldata = contract.commit_action(bytes).calldata().unwrap();
+
+let tx = Eip1559TransactionRequest::new()
+    .from(user_address)
+    .to(contract_address) 
+    .value(0)
+    .gas(900000)
+    .max_fee_per_gas(_gas_fee)
+    .max_priority_fee_per_gas(_gas_fee)
+    .chain_id(chain_id)
+    .nonce(_count)
+    .data(calldata);
+
+let typed_tx: TypedTransaction = TypedTransaction::Eip1559(tx.clone());
+
+let signature = wallet.sign_transaction(&typed_tx).await.unwrap();
+
+let signed_data = TypedTransaction::rlp_signed(&typed_tx, &signature);
+
+let node: TRef<Spatial> = unsafe { ui_node.assume_safe() };
+
+unsafe {
+    node.call("set_signed_data", &[hex::encode(signed_data).to_variant()])
+};
+
+
+NewFuture(Ok(()))
+
+}
+
 
 #[method]
 #[tokio::main]
-async fn make_move(key: PoolArray<u8>, chain_id: u64, time_crystal_contract: GodotString, rpc: GodotString, _gas_fee: u64, _count: u64, action: GodotString, ui_node: Ref<Spatial>) -> NewFuture {
+async fn reveal_action(key: PoolArray<u8>, chain_id: u64, time_crystal_contract: GodotString, rpc: GodotString, _gas_fee: u64, _count: u64, password: GodotString, action: GodotString, ui_node: Ref<Spatial>) -> NewFuture {
 
 let vec = &key.to_vec();
 
@@ -297,7 +361,7 @@ let client = SignerMiddleware::new(provider, wallet.clone());
     
 let contract = TimeCrystalABI::new(contract_address.clone(), Arc::new(client.clone()));
 
-let calldata = contract.make_move(action.to_string()).calldata().unwrap();
+let calldata = contract.reveal_action(password.to_string(), action.to_string()).calldata().unwrap();
 
 let tx = Eip1559TransactionRequest::new()
     .from(user_address)
