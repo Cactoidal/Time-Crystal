4da3b641b749e38da2f095090043a92f4bb762e3
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-11-27 02:36:30 -0500

    Delete contracts/IERC677.sol

--- contracts/IERC677.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
-
-interface IERC677 {
-  function onTokenTransfer(address _sender, uint _value, bytes memory _data) external;
-
-  function onNftTransfer(address _sender, uint _value, bytes memory _data) external;
-}
