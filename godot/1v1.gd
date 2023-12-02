c6a96d294087d20a8bbfd965717c09f3827f6f60
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-12-02 07:29:44 -0500

    Add files via upload

--- godot/1v1.gd
+++ godot/1v1.gd
@@ -96,6 +96,8 @@ func commit_action(action):
 	var secret = action_password + action_id
 	ethers.start_transaction("commit_action", [secret])
 	in_commit_phase = true
+	$Resolution.modulate.a = 1
+	resolution_pause = 1.2
 
 # * #
 func did_player_commit():
@@ -150,9 +152,13 @@ var battle_start_sequence = false
 var battle_start_timer = 60
 var battle_ongoing = false
 var fade_in_battler_stats = false
+var action_prompt_pause = 0
 var player_commit_found = false
 var in_commit_phase = false
+var resolution_pause = 0
 var in_reveal_phase = false
+var show_action_sequence = false
+var damage_number_pause = 0
 
 var fade_in_ending = false
 
@@ -186,6 +192,41 @@ func _process(delta):
 			if $PlayerStats.modulate.a > 1:
 				$PlayerStats.modulate.a = 1
 				fade_in_battler_stats = false
+	
+	if action_prompt_pause == 0:
+		if $ActPrompt.modulate.a > 0:
+			$ActPrompt.modulate.a -= delta/2
+			if $ActPrompt.modulate.a < 0:
+				$ActPrompt.modulate.a = 0
+	else:
+		action_prompt_pause -= delta
+		if action_prompt_pause < 0:
+			action_prompt_pause = 0
+
+	if resolution_pause == 0:
+		if $Resolution.modulate.a > 0:
+			$Resolution.modulate.a -= delta/2
+			if $Resolution.modulate.a < 0:
+				$Resolution.modulate.a = 0
+	else:
+		resolution_pause -= delta
+		if resolution_pause < 0:
+			resolution_pause = 0
+	
+	if damage_number_pause == 0:
+		if get_parent().get_node("WorldRotate/Opponent/Damage").modulate.a > 0:
+			get_parent().get_node("WorldRotate/Opponent/Damage").modulate.a -= delta/2
+			if get_parent().get_node("WorldRotate/Opponent/Damage").modulate.a < 0:
+				get_parent().get_node("WorldRotate/Opponent/Damage").modulate.a = 0
+		
+		if get_parent().get_node("WorldRotate/Player/Damage").modulate.a > 0:
+			get_parent().get_node("WorldRotate/Player/Damage").modulate.a -= delta/2
+			if get_parent().get_node("WorldRotate/Player/Damage").modulate.a < 0:
+				get_parent().get_node("WorldRotate/Player/Damage").modulate.a = 0
+	else:
+		damage_number_pause -= delta
+		if damage_number_pause < 0:
+			damage_number_pause = 0
 	
 	if fade_in_ending == true:
 		if $Overlay.modulate.a < 1:
@@ -221,6 +262,8 @@ func _process(delta):
 			battle_ongoing = true
 			fade_in_battler_stats = true
 			$Scroll/AwaitingOpponent.text = "CHOOSE ACTION...						CHOOSE ACTION...						CHOOSE ACTION...						CHOOSE ACTION...						"
+			$ActPrompt.modulate.a = 1
+			action_prompt_pause = 1.2
 			print("battle started")
 			for card in $Cards.get_children():
 				card.activated = true
@@ -232,7 +275,7 @@ func _process(delta):
 			
 			if find_player_hand == true && hand_wait_simulation_timer <= 0:
 				check_hand()
-				get_hash_monster()
+				#get_hash_monster()
 				simulate_opponent_wait = true
 			
 			if find_opponent == true && opponent_wait_simulation_timer <= 0:
@@ -246,9 +289,12 @@ func _process(delta):
 					did_opponent_commit()
 				else:
 					did_player_commit()
+			
+			if show_action_sequence == true:
+				pass
 				
 				
-			check_timer = 10
+			check_timer = 2
 	
 	if started == true:
 		if got == false:
@@ -291,14 +337,14 @@ func extract():
 												return
 
 var card_selector = 0
-var card_destination = Vector2(73,73)
+var card_destination = Vector2(73,110)
 func card_flip():
 	var new_card = card_flip.instance()
 	new_card.card_info = get_card_info(hand[card_selector])
 	new_card.card_destination = card_destination
 	new_card.ethers = self
 	card_selector += 1
-	card_destination.y += 90
+	card_destination.y += 95
 	$Cards.add_child(new_card)
 
 func set_player_stats(battler_id):
@@ -350,10 +396,18 @@ func resolve_actions(opponent_actions):
 	var player_damage_dealt = int(clamp(player_atk - opponent_block, 10, 10000))
 	var opponent_damage_dealt = int(clamp(opponent_atk - player_block, 10, 10000))
 	
+	damage_number_pause = 2
+	get_parent().get_node("WorldRotate/Opponent/Damage").text = "-" + String(player_damage_dealt) + " HP"
+	get_parent().get_node("WorldRotate/Player/Damage").text = "-" + String(opponent_damage_dealt) + " HP"
+	get_parent().get_node("WorldRotate/Player/Damage").modulate.a = 1
+	get_parent().get_node("WorldRotate/Opponent/Damage").modulate.a = 1
+	
 	opponent_hp = int(clamp(opponent_hp - player_damage_dealt, 0, 10000))
 	player_hp = int(clamp(player_hp - opponent_damage_dealt, 0, 10000))
 	player_energy += 1 + player_action["gain"]
 	opponent_energy += 1 + opponent_action["gain"]
+	
+
 	
 	$PlayerStats/HP.text = "HP: " + String(player_hp) + " / " + String(player_max_hp)
 	$OpponentStats/HP.text = "HP: " + String(opponent_hp) + " / " + String(opponent_max_hp)
@@ -366,6 +420,8 @@ func resolve_actions(opponent_actions):
 		$Overlay.visible = true
 		fade_in_ending = true
 	else:
+		action_prompt_pause = 1.2
+		$ActPrompt.modulate.a = 1
 		$Scroll/AwaitingOpponent.text = "CHOOSE ACTION...						CHOOSE ACTION...						CHOOSE ACTION...						CHOOSE ACTION...						"
 		for card in $Cards.get_children():
 			card.activated = true
@@ -377,26 +433,26 @@ func end_game():
 func get_card_info(card_id):
 	match card_id:
 		10: return {"id": "10", "type": "normal", "name": "Laser", "attack": 1, "defense": 0, "cost": 0, "gain": 0, "counter_bonus": 2}
-		11: return {"id": "11", "type": "normal", "name": "Energy Wave", "attack": 1, "defense": 0, "cost": 0, "gain": 1, "counter_bonus": 0}
+		11: return {"id": "11", "type": "normal", "name": "Energy\nWave", "attack": 1, "defense": 0, "cost": 0, "gain": 1, "counter_bonus": 0}
 		12: return {"id": "12", "type": "normal", "name": "Pulsar", "attack": 1, "defense": 0, "cost": 1, "gain": 0, "counter_bonus": 5}
 		13: return {"id": "13", "type": "normal", "name": "Bonk", "attack": 1, "defense": 0, "cost": 0, "gain": 0, "counter_bonus": 2}
 		14: return {"id": "14", "type": "normal", "name": "Irradiate", "attack": 1, "defense": 0, "cost": 0, "gain": 1, "counter_bonus": 0}
-		15: return {"id": "15", "type": "normal", "name": "Cold Glow", "attack": 0, "defense": 1, "cost": 0, "gain": 1, "counter_bonus": 0}
-		16: return {"id": "16", "type": "normal", "name": "Ice Barrier", "attack": 0, "defense": 1, "cost": 0, "gain": 1, "counter_bonus": 0}
-		17: return {"id": "17", "type": "normal", "name": "Sun Guard", "attack": 1, "defense": 1, "cost": 1, "gain": 0, "counter_bonus": 2}
+		15: return {"id": "15", "type": "normal", "name": "Cold\nGlow", "attack": 0, "defense": 1, "cost": 0, "gain": 1, "counter_bonus": 0}
+		16: return {"id": "16", "type": "normal", "name": "Ice\nBarrier", "attack": 0, "defense": 1, "cost": 0, "gain": 1, "counter_bonus": 0}
+		17: return {"id": "17", "type": "normal", "name": "Sun\nGuard", "attack": 1, "defense": 1, "cost": 1, "gain": 0, "counter_bonus": 2}
 		18: return {"id": "18", "type": "normal", "name": "Crunch", "attack": 2, "defense": 0, "cost": 1, "gain": 0, "counter_bonus": 4}
-		19: return {"id": "19", "type": "normal", "name": "Rainbow Shroud", "attack": 1, "defense": 2, "cost": 2, "gain": 1, "counter_bonus": 1}
+		19: return {"id": "19", "type": "normal", "name": "Rainbow\nShroud", "attack": 1, "defense": 2, "cost": 2, "gain": 1, "counter_bonus": 1}
 		20: return {"id": "20", "type": "normal", "name": "Aurora", "attack": 1, "defense": 2, "cost": 1, "gain": 0, "counter_bonus": 0}
-		21: return {"id": "21", "type": "power", "name": "Power Beam", "attack": 1, "defense": 0, "cost": 2, "gain": 0, "counter_bonus": 2}
+		21: return {"id": "21", "type": "power", "name": "Power\nBeam", "attack": 1, "defense": 0, "cost": 2, "gain": 0, "counter_bonus": 2}
 		22: return {"id": "22", "type": "power", "name": "Rust", "attack": 1, "defense": 1, "cost": 2, "gain": 0, "counter_bonus": 0}
-		23: return {"id": "23", "type": "power", "name": "Crystallize", "attack": 1, "defense": 1, "cost": 2, "gain": 0, "counter_bonus": 0}
+		23: return {"id": "23", "type": "power", "name": "Fracture", "attack": 1, "defense": 1, "cost": 2, "gain": 0, "counter_bonus": 0}
 		24: return {"id": "24", "type": "power", "name": "Disrupt", "attack": 1, "defense": 0, "cost": 2, "gain": 0, "counter_bonus": 5}
 		25: return {"id": "25", "type": "power", "name": "Explosion", "attack": 2, "defense": 0, "cost": 3, "gain": 0, "counter_bonus": 0}
 		26: return {"id": "26", "type": "power", "name": "Crystal Laser", "attack": 1, "defense": 1, "cost": 2, "gain": 0, "counter_bonus": 0}
-		27: return {"id": "27", "type": "power", "name": "Distintegrate", "attack": 2, "defense": 0, "cost": 3, "gain": 0, "counter_bonus": 0}
-		28: return {"id": "28", "type": "power", "name": "Void Shield", "attack": 1, "defense": 2, "cost": 3, "gain": 0, "counter_bonus": 2}
-		29: return {"id": "29", "type": "power", "name": "Seeker Missile", "attack": 2, "defense": 0, "cost": 3, "gain": 0, "counter_bonus": 3}
-		30: return {"id": "30", "type": "energy", "name": "Energy Charge", "attack": 0, "defense": 0, "cost": 0, "gain": 3, "counter_bonus": 0}
+		27: return {"id": "27", "type": "power", "name": "Destroy", "attack": 2, "defense": 0, "cost": 3, "gain": 0, "counter_bonus": 0}
+		28: return {"id": "28", "type": "power", "name": "Void\nShield", "attack": 1, "defense": 2, "cost": 3, "gain": 0, "counter_bonus": 2}
+		29: return {"id": "29", "type": "power", "name": "Seeker\nMissile", "attack": 2, "defense": 0, "cost": 3, "gain": 0, "counter_bonus": 3}
+		30: return {"id": "30", "type": "energy", "name": "Energy\nCharge", "attack": 0, "defense": 0, "cost": 0, "gain": 3, "counter_bonus": 0}
 		99: return {"id": "99", "type": "normal", "name": "Attack", "attack": 1, "defense": 0, "cost": 0, "gain": 0, "counter_bonus": 0}
 		
 func get_battler_info(battler_id):
